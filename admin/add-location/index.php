46e465796de9a7825c7f5db8455b304a1089d41e
Jay-Ubisse <joaquimubisse@gmail.com>
2023-03-20 16:29:06 +0200

    add location add functionality

--- admin/add-location/index.php
+++ admin/add-location/index.php
@@ -14,20 +14,25 @@
 </head>
 <body>
     <header class="bg-orange-700 text-white font-semibold flex flex-row justify-between items-center px-7 py-4">
-            <div class="text-lg">
-                <?php
-                    echo $_SESSION['user-data']['username'];
-                ?>
+        <div class="text-lg">
+            <?php
+                echo $_SESSION['user-data']['username'];
+            ?>
+        </div>
+        <div class="flex flex-row gap-4">
+            <div class="border border-white rounded-md px-5 py-2 bg-orange-700 hover:bg-orange-500">
+                <a href="../home/">Dashboard</a>
             </div>
             <div class="border border-white rounded-md px-5 py-2 bg-orange-700 hover:bg-orange-500">
                 <a href="../../server/src/logout-route.php">Sair</a>
             </div>
+        </div>
     </header>
     <main class="bg-gray-200 p-1 h-[calc(100vh-100px)] overflow-scroll">
         <section class="bg-white w-5/6 h-fit mx-auto my-20 p-6 rounded-lg">
             <h1 class="text-orange-700 text-xl font-semibold border-b border-orange-700 w-fit">DASHBOARD</h1>
             <div class="flex flex-row justify-evenly h-44 mt-10">
-                <a href="#" class="border-2 border-orange-700 rounded-md w-[30%] h-full flex justify-center items-center text-orange-700 text-xl font-bold hover:bg-orange-700 hover:text-white">
+                <a href="./location/district/" class="border-2 border-orange-700 rounded-md w-[30%] h-full flex justify-center items-center text-orange-700 text-xl font-bold hover:bg-orange-700 hover:text-white">
                     Adicionar Distrito
                 </a>
                 <a href="#" class="border-2 border-orange-700 rounded-md w-[30%] h-full flex justify-center items-center text-orange-700 text-xl font-bold hover:bg-orange-700 hover:text-white">
---
+++ server/src/add-location/district.php
@@ -0,0 +1,48 @@
+<?php
+require "../../config/connect.php";
+session_start();
+
+$province = $_POST['province'];
+$district = trim($_POST['district']);
+
+$checkDistrictQuery = "SELECT * FROM pocsquare.districts WHERE province = '$province'";
+$checkDistrictResult = $dbcon->query($checkDistrictQuery );
+
+$rows = $checkDistrictResult->fetchAll(PDO::FETCH_ASSOC);
+
+$isFound = false;
+foreach ($rows as $row) {
+    if(strcasecmp($district , $row['district_name']) == 0) {
+        $isFound = true;
+        $_SESSION['add-province'] = "Este distrito já foi adicionado!";
+        $_SESSION['error'] = true;
+        header("location: ../../../admin/add-location/location/district/");
+    }
+}
+
+if(!$isFound) {
+    try {
+
+        $dbcon->beginTransaction();
+    
+        $saveDistrictQuery = "INSERT INTO pocsquare.districts 
+                                        (province, district_name) 
+                                        VALUES (?, ?)";
+        $stmt = $dbcon->prepare($saveDistrictQuery);
+        $stmt->execute([$province, $district]);
+    
+    
+        $dbcon->commit();
+        
+        $_SESSION['add-province'] = "Distrito adicionado com sucesso!";
+        $_SESSION['error'] = false;
+        header("location: ../../../admin/add-location/location/district/");
+    
+    } 
+    catch(PDOException $ex) {
+        //Something went wrong rollback!
+        $dbcon->rollBack();
+        echo $ex->getMessage();
+    }
+}
+?>
