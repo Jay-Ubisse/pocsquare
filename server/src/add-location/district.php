e5026d80cba93e1efbc0e4e52dd30c821955c0f7
Jay-Ubisse <joaquimubisse@gmail.com>
2023-03-28 21:01:11 +0200

    update functionality to add administrative posts

--- server/src/add-location/district.php
+++ server/src/add-location/district.php
@@ -5,29 +5,136 @@ session_start();
 $province = $_POST['province'];
 $district = trim($_POST['district']);
 
-$checkDistrictQuery = "SELECT * FROM jayubiss_pocsquareocs.districts WHERE province = '$province'";
-$checkDistrictResult = $dbcon->query($checkDistrictQuery);
-
-$rows = $checkDistrictResult->fetchAll(PDO::FETCH_ASSOC);
-
-$isFound = false;
-foreach ($rows as $row) {
-    if(strcasecmp($district , $row['district_name']) == 0) {
-        $isFound = true;
-        $_SESSION['registration-info'] = "Este distrito já foi adicionado!";
-        $_SESSION['error'] = true;
-        header("location: ../../../admin/add-location/location/district/");
-    }
+switch ($province) {
+    case 'Maputo Cidade':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.mc_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("mc_district");
+        }
+        break;
+    case 'Maputo Província':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.mp_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("mp_district");
+        }
+        break;
+    case 'Gaza':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.gz_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("gz_district");
+        }
+        break;
+    case 'Inhambane':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.in_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("in_district");
+        }
+        break;
+    case 'Manica':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.mn_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("mn_district");
+        }
+        break;
+    case 'Sofala':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.sf_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("sf_district");
+        }
+        break;
+    case 'Tete':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.tt_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("tt_district");
+        }
+        break;
+    case 'Nampula':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.np_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("np_district");
+        }
+        break;
+    case 'Niassa':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.ns_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("ns_district");
+        }
+        break;
+    case 'Zambézia':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.zb_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("zb_district");
+        }
+        break;
+    case 'Cabo Delgado':
+        $checkDistrict = "SELECT * FROM jayubiss_pocsquareocs.cd_district";
+        $isFound = checkDistrict($checkDistrict);
+        if($isFound) {
+            errorMEssage();
+        } else {
+            saveDistrict("cd_district");
+        }
+        break;
+    default:
+        # code...
+        break;
 }
 
-if(!$isFound) {
+
+function checkDistrict($checkDistrict) {
+    global $district, $province, $dbcon;
+
+    $checkDistrictResult = $dbcon->query($checkDistrict);
+    $rows = $checkDistrictResult->fetchAll(PDO::FETCH_ASSOC);
+
+    $isFound = false;
+    foreach ($rows as $row) {
+        if(strcasecmp($district , $row['district_name']) == 0) {
+            $isFound = true;
+        }
+    }
+
+    return $isFound;
+}
+
+function saveDistrict($table) {
+    global $district, $province, $dbcon;
+
     try {
 
         $dbcon->beginTransaction();
     
-        $saveDistrictQuery = "INSERT INTO jayubiss_pocsquareocs.districts 
-                                        (province, district_name) 
-                                        VALUES (?, ?)";
+        $saveDistrictQuery = "INSERT INTO jayubiss_pocsquareocs." . $table . " (province, district_name) VALUES (?, ?)";
         $stmt = $dbcon->prepare($saveDistrictQuery);
         $stmt->execute([$province, $district]);
     
@@ -45,4 +152,11 @@ if(!$isFound) {
         echo $ex->getMessage();
     }
 }
+
+function errorMessage() {
+    $_SESSION['registration-info'] = "Estdistrito já foi adicionado!";
+    $_SESSION['error'] = true;
+    header("location: ../../../admin/add-location/location/district/");
+}
+
 ?>
